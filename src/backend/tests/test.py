659d1e46de5f6c91630876c9ef35bad3c9a50e26
LuCC-que <chen1968493689@gmail.com>
2022-09-22 18:06:51 -0400

    Test files are created
    I create some test files for the database, there are more tests need to be done;
    they are their just to check if the database are working as expect;

--- src/backend/tests/test.py
+++ src/backend/tests/test.py
@@ -1,15 +1,9 @@
-from flask import Flask
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tests/data.db'
-
-@app.before_first_request
-def create_tables():
-  print("=================tables are created======================")
-  db.create_all()
-
-
-if __name__ == '__main__':
-  from db import db
-  db.init_app(app)
-  app.run(port=8000, debug=True)
+from generator.data_generator import tables
+def main():
+  print("==========TESTING===========")
+  print(tables)
+  
+  
+  
+  
+main()
