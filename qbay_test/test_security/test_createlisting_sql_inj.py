5b15c15ad889e73dc06d9d3e91d9d43994a04eff
Penelope-Liang <yuanqiliang50@yahoo.com>
2022-11-18 22:41:31 -0500

    fix the problem of test_createlisting_sql_inj.py

--- qbay_test/test_security/test_createlisting_sql_inj.py
+++ qbay_test/test_security/test_createlisting_sql_inj.py
@@ -18,7 +18,7 @@ class Security_Of_SQL_Injection_Tes(BaseCase):
         for line in lines:
             correct_createlisting = {
                 "#posted_date": date.today(),
-                "#title": "Fine Town",
+                "#title": "Nice Town House",
                 "#description": "Relax with the whole family at this peaceful \
                                 5 Bedroom Villa overlooking Hout Bay Beach",
                 "#price": 1500,
@@ -26,67 +26,67 @@ class Security_Of_SQL_Injection_Tes(BaseCase):
                 "#capacity": 4
             }
     
-        for key1 in correct_createlisting:
-            self.open(base_url + "/login")
-            self.type("#email", "111@111.com")
-            self.type("#password", "123!Abc")
-            self.click("#btn-submit")
-            self.click("a[href='/create'")
-            self.type(key1, line)
+            for key1 in correct_createlisting:
+                self.open(base_url + "/login")
+                self.type("#email", "111@111.com")
+                self.type("#password", "123!Abc")
+                self.click("#btn-submit")
+                self.click("a[href='/create'")
+                self.type(key1, line)
 
-            for key2, value in correct_createlisting.items():
-                if key2 != key1:
-                    self.type(key2, value)
+                for key2, value in correct_createlisting.items():
+                    if key2 != key1:
+                        self.type(key2, value)
 
-            try:
-                self.click("#submit")
-                listing = {
-                    "email": "111@111.com",
-                    "posted_date": correct_createlisting["#posted_date"],
-                    "title": correct_createlisting["#title"],
-                    "description": correct_createlisting["#description"],
-                    "price": correct_createlisting["#price"],
-                    "address": correct_createlisting["#address"],
-                    "capacity": correct_createlisting["#capacity"]
-                }
+                try:
+                    self.click("#submit")
+                    listing = {
+                        "email": "111@111.com",
+                        "posted_date": correct_createlisting["#posted_date"],
+                        "title": correct_createlisting["#title"],
+                        "description": correct_createlisting["#description"],
+                        "price": correct_createlisting["#price"],
+                        "address": correct_createlisting["#address"],
+                        "capacity": correct_createlisting["#capacity"]
+                    }
 
-                if key1 == "posted_date":
-                    listing["posted_date"] = line
-                elif key1 == "title":
-                    listing["title"] = line
-                elif key1 == "description":
-                    listing["description"] = line
-                elif key1 == "price":
-                    listing["price"] = line
-                elif key1 == "address":
-                    listing["address"] = line
-                elif key1 == "capacity":
-                    listing["capacity"] = line
-                
-                create_listing_format_checker(listing)
+                    if key1 == "posted_date":
+                        listing["posted_date"] = line
+                    elif key1 == "title":
+                        listing["title"] = line
+                    elif key1 == "description":
+                        listing["description"] = line
+                    elif key1 == "price":
+                        listing["price"] = line
+                    elif key1 == "address":
+                        listing["address"] = line
+                    elif key1 == "capacity":
+                        listing["capacity"] = line
+                    
+                    create_listing_format_checker(listing)
 
-                path = os.path.abspath(os.getcwd())
-                connection = sqlite3.connect(path + "/qbay/data.db")
-                cursor = connection.cursor()
-                cursor.execute("SELECT * \
-                               FROM Properties WHERE posted_date=? \
-                               and title=?\
-                               and description=? \
-                               and price = ? \
-                               and address = ?\
-                               and capacity = ?", 
-                               (listing["posted_date"],
-                                listing["title"],
-                                listing["description"],
-                                listing["price"],
-                                listing["address"],
-                                listing["capacity"]))
-                row = cursor.fetchone()
-                connection.close()
+                    path = os.path.abspath(os.getcwd())
+                    connection = sqlite3.connect(path + "/qbay/data.db")
+                    cursor = connection.cursor()
+                    cursor.execute("SELECT * \
+                                FROM Properties WHERE posted_date=? \
+                                and title=?\
+                                and description=? \
+                                and price = ? \
+                                and address = ?\
+                                and capacity = ?", 
+                                (listing["posted_date"],
+                                    listing["title"],
+                                    listing["description"],
+                                    listing["price"],
+                                    listing["address"],
+                                    listing["capacity"]))
+                    row = cursor.fetchone()
+                    connection.close()
 
-                if row is None:
-                    raise Exception("Injection exists")
-                assert True
+                    if row is None:
+                        raise Exception("Injection exists")
+                    assert True
 
-            except Exception:
-                assert False
+                except Exception:
+                    assert False
